70718bdbd408a56e75ceee2b6fa7a1d8d110fd47
Diljot56 <diljotsingh6156@gmail.com>
2026-06-29 15:12:02 +0530

    Add: trapping-rain-water - submission-2

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-2.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        // int n = height.size();
+        // vector<int> prefix(n);
+        // vector<int> suffix(n);
+        // prefix[0] = height[0];
+        // suffix[n - 1] = height[n - 1];
+
+        // for(int i = 1; i < n; i++){
+        //     prefix[i] = max(prefix[i - 1], height[i]);
+        // }
+
+        // for(int i = n - 2; i >= 0; i--){
+        //     suffix[i] = max(suffix[i + 1], height[i]);
+        // }
+
+        // int total = 0;
+        // for(int i = 0; i < n; i++){
+        //     int leftMax = prefix[i];
+        //     int rightMax = suffix[i];
+        //     if(height[i] < leftMax && height[i] < rightMax){
+        //         total += min(leftMax, rightMax) - height[i];
+        //     }
+        // }
+        // return total;
+
+        int left = 0;
+        int right = height.size() - 1;
+        int leftMax = 0;
+        int rightMax = 0;
+        int total = 0;
+        while(left < right){
+            if(height[left] < height[right]){
+                if(height[left] >= leftMax){
+                    leftMax = height[left];
+                }
+                else{
+                    total += leftMax - height[left];
+                }
+                left++;
+            }
+            else{
+                if(height[right] >= rightMax){
+                    rightMax = height[right];
+                }
+                else{
+                    total += rightMax - height[right];
+                }
+                right--;
+            }
+        } 
+        return total;
+    }
+};
