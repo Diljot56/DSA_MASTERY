96da889217b70c0d345ca7e22b97ffb0b54bb3d8
Diljot56 <diljotsingh6156@gmail.com>
2026-07-21 11:39:48 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+
+    int find(vector<int> &dp, int n){
+        if(n <= 2){
+            return n;
+        }
+        if(dp[n] != -1) return dp[n];
+        return dp[n] = find(dp, n - 1) + find(dp, n - 2);
+    }   
+
+    int climbStairs(int n){
+        vector<int> dp(n + 1, -1);
+        return find(dp, n);
+    }
+};
