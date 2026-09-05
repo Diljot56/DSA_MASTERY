4575e1c59dab05aff94dfd8ab348fa1e683e8270
Diljot56 <diljotsingh6156@gmail.com>
2026-09-05 15:23:53 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int m = 0;
+        int l = 0;
+        for(int r = 1; r < prices.size(); r++){
+            if(prices[l] < prices[r]){
+                m = max(m, prices[r] - prices[l]);
+            }
+            else{
+                l = r;
+            }
+        }
+        return m;
+    }
+};
