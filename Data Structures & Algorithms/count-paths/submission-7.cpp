34ceb5feb13bf96995fb0bf67403b60619eb4021
Diljot56 <diljotsingh6156@gmail.com>
2026-09-10 21:12:22 +0530

    Add: count-paths - submission-7

---
+++ Data Structures & Algorithms/count-paths/submission-7.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    // int find(int i, int j, int m, int n, vector<vector<int>> &dp){
+    //     if(i >= m || j >= n){
+    //         return 0;
+    //     }
+    //     if(i == m - 1 && j == n - 1){
+    //         return 1;
+    //     }
+    //     if(dp[i][j] != -1){
+    //         return dp[i][j];
+    //     }
+    //     int right = find(i, j + 1, m, n, dp);
+    //     int down = find(i + 1, j, m , n, dp);
+    //     return dp[i][j] = right + down;
+    // }
+
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        //return find(0, 0, m, n, dp);
+        dp[m - 1][n - 1] = 1;
+        for(int i = m - 1; i >= 0; i--){
+            for(int j = n - 1; j >= 0; j--){
+                if(i == m - 1 && j == n - 1)continue;
+                int down = dp[i + 1][j];
+                int right = dp[i][j + 1];
+                dp[i][j] = down + right;
+            }
+        }
+        return dp[0][0];
+    }
+};
